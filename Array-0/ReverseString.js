4f375e8e49eee31197e4a547b2c7195c130f6282
Nihal <nihal301298@gmail.com>
2025-06-09 22:18:27 +0530

    solution

--- Array-0/ReverseString.js
+++ Array-0/ReverseString.js
@@ -7,4 +7,15 @@
 // Input: s = ["h","e","l","l","o"]
 // Output: ["o","l","l","e","h"]
 
-//
+// solution 
+
+var reverseString = function(s) {
+    let x = s.length-1;
+    for(let i =0; i<Math.floor(s.length/2); i++){
+        let temp = s[i];
+        s[i]= s[x];
+        s[x] = temp;
+        x--;
+    }
+    return s;
+};
