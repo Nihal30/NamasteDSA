fce3886003d02827eeb13ec3dd0682f6983979d3
Nihal <nihal301298@gmail.com>
2025-06-16 22:54:43 +0530

    optimal

--- Array-0/moveZero.js
+++ Array-0/moveZero.js
@@ -7,3 +7,24 @@
 
 // Input: nums = [0,1,0,3,12]
 // Output: [1,3,12,0,0]
+
+// optimal using 2 pointers 
+
+var moveZeroes = function(nums) {
+      let x = 0;
+      for (let i = 0; i < nums.length; i++) {
+          if (nums[i] !== 0) {
+              nums[x] = nums[i];
+              x++;
+          }
+      }
+      for (let i = x; i < nums.length; i++) {
+          nums[i] = 0;
+      }
+
+}
+
+
+let nums =[0,1,0,3,12]
+moveZeroes(nums)    
+
